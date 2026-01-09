476045b5e9988c898fec64a325d3e3d03daf2252
“Chong <2907132C@student.gla.ac.uk>
2026-01-09 16:18:49 +0800

    refactor for new domain name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base : '/DewPage-2/',
+    base : '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
